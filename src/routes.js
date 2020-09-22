a0a923de299703bab4f981c8a7b8889f61451faa
Bruno Reolon <bruno.reolonn@gmail.com>
2020-09-21 21:14:11 -0300

    create home

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,15 @@ import { BrowserRouter,Route, Switch } from 'react-router-dom';
 import Logon from './pages/Login'
 import CadastroUsuario from './pages/CadastroUsuario';
 import CadastroDemanda from './pages/CadastroDemanda';
+import Home from './pages/Home';
 
 export default function Routes() {
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Logon} />
+                <Route path="/" exact component={Home} />
+                <Route path="/home" exact component={Home} />
+                <Route path="/logon" component={Logon} />
                 <Route path="/cadastro-usuario" component={CadastroUsuario} />
                 <Route path="/cadastro-demanda" component={CadastroDemanda} />
             </Switch>
